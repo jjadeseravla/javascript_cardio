3186ba49f6cb2994ec4110158b4d7c9d2a93c636
jjade seravla <jadealvares@hotmail.co.uk>
2018-03-08 11:37:22 +0000

    ex 1 solutions

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,7 +3,41 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  //return str.split("").reverse().join("");
+
+  // let revString = "";
+  // for(var i = str.length -1; i >= 0; i-- ) {
+  //   revString = revString + str[i];
+  // }
+  // return revString;
+
+  // let revString = "";
+  // for(var i =0; i <= str.length -1; i++ ) {
+  //   revString = str[i] + revString;
+  // }
+  // return revString;
+
+  // let revString = "";
+  // for(let char of str ) {
+  //   revString = char + revString;
+  // }
+  // return revString;
+
+  // let revString = "";
+  // str.split("").forEach(function(char) {
+  //   revString = char + revString;
+  // });
+  // return revString;
+
+  // let revString = "";
+  // str.split("").forEach(char => revString = char + revString);
+  // return revString;
+
+  // return str.split("").reduce(function(revString, char) {
+  //   return char + revString;
+  // }, "");
+
+  return str.split("").reduce((revString, char) => char + revString, "");
 }
 
 
