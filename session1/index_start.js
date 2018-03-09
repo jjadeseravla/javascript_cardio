571fb5b4dec285d0a8cd63a7fe1d44e902100699
jjade seravla <jadealvares@hotmail.co.uk>
2018-03-09 10:53:58 +0000

    last exercise done

--- session1/index_start.js
+++ session1/index_start.js
@@ -74,6 +74,10 @@ function capitalizeLetters(str) {
   //   return word[0].toUpperCase() + word.substr(1);
   // }).join(" ");
 
+  // return str.replace(/\b[a-z]/gi, function(char) {
+  //   return char.toUpperCase();
+  // });
+
   return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.substr(1)).join(" ");
 }
 
@@ -82,13 +86,43 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = "";
+  str.split("").forEach(function(char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+  for(let char in charMap) {
+    if(charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxhar = char;
+    }
+  }
+  return maxChar;
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if( i % 3 === 0 && i % 5 === 0 ) {
+      console.log('FizzBuzz');
+    } else if(i % 3 === 0) {
+      console.log('Fizz');
+    } else if(i % 5 === 0){
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 
 
@@ -96,6 +130,7 @@ function fizzBuzz() {}
 //const output = reverseString('hello');
 //const output = isPalindrome('hello');
 //const output = reverseInt(-121);
-const output = capitalizeLetters("i love coding");
+//const output = capitalizeLetters("i love coding");
+const output = maxCharacter("javascript");
 
 console.log(output);
